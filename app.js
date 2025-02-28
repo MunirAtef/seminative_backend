ab8ce063bd4ff99ff6088caa90157de447e95e5d
Munir M. Atef <Mounir.Muhammad20@fcai.usc.edu.eg>
2025-02-28 21:50:38 +0200

    ADD Alarm Service

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const {mongodbConnect} = require('./src/features/connection.js');
+const axios = require('axios')
 
 const authRouter = require('./src/features/auth/authRoute');
 const storeRouter = require('./src/features/store/storeRoute');
@@ -20,6 +21,17 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/auth', authRouter);
+
+// for ping service
+app.get('/ping', async (req, res) => {
+    console.log('Received request at Main Server');
+    res.send('Received at Main Server, forwarding after 30 sec...');
+    setTimeout(async () => {
+        console.log('Forwarding request from Main to Secondary');
+        await axios.get('https://alarm-service-k9me.onrender.com/ping').catch(err => console.error('Error:', err.message));
+    }, 30000);
+});
+
 
 app.use(authMiddleware);
 app.use('/store', storeRouter);
